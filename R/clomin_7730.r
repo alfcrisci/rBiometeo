08a02b6a9bd6e83dda03b68fdea3dac3e6a9aefd
Alfonso <alfcrisci@gmail.com>
2017-09-01 16:43:59 +0200

    Update clomin_7730.r

--- R/clomin_7730.r
+++ R/clomin_7730.r
@@ -7,8 +7,6 @@
 #' @param numeric wind Wind speed in meter per second.
 #' @param numeric tr Mean radiant temperature in Celsius degrees.
 #' @param numeric M Metabolic rate of subject in Watt per mq.
-#' @param character outcome Type of outcome expected. Two parameters are available "clomax" and "clomin".
-
 #' @return 
 #'
 #'
@@ -22,12 +20,12 @@
 #'
 
 clomin_7730=function(t,rh,wind,tr,M) {
-                          ct$assign("ta", as.array(t))
+                         ct$assign("ta", as.array(t))
                          ct$assign("rh", as.array(rh))
                          ct$assign("vel", as.array(wind))
                          ct$assign("tr", as.array(tr))
                          ct$assign("M", as.array(M))
-                         ct$eval("var res=[]; for(var i=0, len=outcome.length; i < len; i++){ res[i]=clomin_7730(outcome[i],ta[i],rh[i],vel[i],tr[i],M[0])};")
+                         ct$eval("var res=[]; for(var i=0, len=ta.length; i < len; i++){ res[i]=clomin_7730(ta[i],rh[i],vel[i],tr[i],M[0])};")
                          res=ct$get("res")
                          return(ifelse(res==9999,NA,res))
 }

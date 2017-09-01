08fc2955a1f04fd5fc5ba7a8a5a5b1fe50b471ce
Alfonso <alfcrisci@gmail.com>
2017-09-01 20:36:08 +0200

    Update clomin_7730.r

--- R/clomin_7730.r
+++ R/clomin_7730.r
@@ -7,6 +7,7 @@
 #' @param numeric wind Wind speed in meter per second.
 #' @param numeric tr Mean radiant temperature in Celsius degrees.
 #' @param numeric M Metabolic rate of subject in Watt per mq.
+#' @param numeric W Mechanical work of subject in Watt per mq. Generally 0.
 #' @return 
 #'
 #'
@@ -25,7 +26,8 @@ clomin_7730=function(t,rh,wind,tr,M) {
                          ct$assign("vel", as.array(wind))
                          ct$assign("tr", as.array(tr))
                          ct$assign("M", as.array(M))
-                         ct$eval("var res=[]; for(var i=0, len=ta.length; i < len; i++){ res[i]=clomin_7730(ta[i],rh[i],vel[i],tr[i],M[0])};")
+                         ct$assign("M", as.array(W))
+                         ct$eval("var res=[]; for(var i=0, len=ta.length; i < len; i++){ res[i]=clomin_7730(ta[i],rh[i],vel[i],tr[i],M[0],W[0])};")
                          res=ct$get("res")
                          return(ifelse(res==9999,NA,res))
 }

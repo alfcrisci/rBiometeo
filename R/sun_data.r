8c3b3ef0bd725e1f9ffc2a925bb7f938ebcab146
Alfonso <alfcrisci@gmail.com>
2017-09-01 14:30:48 +0200

    Update sun_data.r

--- R/sun_data.r
+++ R/sun_data.r
@@ -2,7 +2,7 @@
 #'
 #' Calculate solar parameter for a location and a time.
 #'
-#' @param datetime  Datetime in format YYYY-MM-DD HH:MM:SS. 
+#' @param datetime  Datetime as in "%Y-%m-%d %H:%M:%S" format
 #' @param numeric lat Latitude in decimal degrees.
 #' @param numeric lon Longitude in decimal degrees.
 #' @param character parameter Six solar parameter are available by name "azimuth","zenith","solarZenith","elevation","declination","JD".
@@ -18,15 +18,13 @@
 #'
 #'
 
-sun_data=function(datetime,lat,lon,parameter="azimuth") {
-                         datetimestring=format(datetime,"%Y-%m-%dT%H:%M:%S") 
-                         if ( length( datetimestring)>1) {parameter=rep(parameter,length(datetimestring))}
-  
+sun_data=function(datetime,lat,lon,parameter="azimuth",tz="GMT") {
+                         datetimestring=format(as.POSIXct(datetime,tz=tz),"%Y-%m-%dT%H:%M:%S") 
                          ct$assign("datetime", as.array(datetimestring))
                          ct$assign("lat", as.array(lat))
                          ct$assign("lon", as.array(lon))
                          ct$assign("parameter", as.array(parameter))
-                         ct$eval("var res=[]; for(var i=0, len=datetime.length; i < len; i++){ res[i]=sun_data(datetime[i],lat[i],lon[i],parameter[i])};")
+                         ct$eval("var res=[]; for(var i=0, len=datetime.length; i < len; i++){ res[i]=sun_data(datetime[i],lat[1],lon[1],parameter[1])};")
                          res=ct$get("res")
                          return(ifelse(res==9999,NA,res))
 }

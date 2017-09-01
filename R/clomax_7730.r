c81ff404384fa3a21dab221748c9c559b8fead11
Alfonso <alfcrisci@gmail.com>
2017-09-01 16:43:01 +0200

    Update clomax_7730.r

--- R/clomax_7730.r
+++ R/clomax_7730.r
@@ -7,7 +7,6 @@
 #' @param numeric wind Wind speed in meter per second.
 #' @param numeric tr Mean radiant temperature in Celsius degrees.
 #' @param numeric M Metabolic rate of subject in Watt per mq.
-#' @param character outcome Type of outcome expected. Two parameters are available "clomax" and "clomin".
 
 #' @return 
 #'
@@ -27,7 +26,7 @@ clomax_7730=function(t,rh,wind,tr,M) {
                          ct$assign("vel", as.array(wind))
                          ct$assign("tr", as.array(tr))
                          ct$assign("M", as.array(M))
-                         ct$eval("var res=[]; for(var i=0, len=outcome.length; i < len; i++){ res[i]=clomax_7730(outcome[i],ta[i],rh[i],vel[i],tr[i],M[0])};")
+                         ct$eval("var res=[]; for(var i=0, len=ta.length; i < len; i++){ res[i]=clomax_7730(ta[i],rh[i],vel[i],tr[i],M[0])};")
                          res=ct$get("res")
                          return(ifelse(res==9999,NA,res))
 }

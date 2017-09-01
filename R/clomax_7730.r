2322f1d7d467d7555ea56a029b9ea99c0c5ffbac
Alfonso <alfcrisci@gmail.com>
2017-09-01 20:33:26 +0200

    Update clomax_7730.r

--- R/clomax_7730.r
+++ R/clomax_7730.r
@@ -7,7 +7,7 @@
 #' @param numeric wind Wind speed in meter per second.
 #' @param numeric tr Mean radiant temperature in Celsius degrees.
 #' @param numeric M Metabolic rate of subject in Watt per mq.
-
+#' @param numeric W Mechanical work of subject in Watt per mq. Generally 0.
 #' @return 
 #'
 #'
@@ -20,13 +20,14 @@
 #'
 #'
 
-clomax_7730=function(t,rh,wind,tr,M) {
+clomax_7730=function(t,rh,wind,tr,M,W) {
                          ct$assign("ta", as.array(t))
                          ct$assign("rh", as.array(rh))
                          ct$assign("vel", as.array(wind))
                          ct$assign("tr", as.array(tr))
                          ct$assign("M", as.array(M))
-                         ct$eval("var res=[]; for(var i=0, len=ta.length; i < len; i++){ res[i]=clomax_7730(ta[i],rh[i],vel[i],tr[i],M[0])};")
+                         ct$assign("W", as.array(W))
+                         ct$eval("var res=[]; for(var i=0, len=ta.length; i < len; i++){ res[i]=clomax_7730(ta[i],rh[i],vel[i],tr[i],M[0],W[0])};")
                          res=ct$get("res")
                          return(ifelse(res==9999,NA,res))
 }

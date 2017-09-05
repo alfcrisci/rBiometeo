ca12b93a6d8fc2f19105fcbd09baee8601cccd7c
alfcrisci <alfcrisci@gmail.com>
2017-09-05 12:02:51 +0200

    reframe pmv functs

--- R/PMV_ISO7730.r
+++ R/PMV_ISO7730.r
@@ -24,12 +24,12 @@
 PMV_ISO7730=function(t,rh,wind,tr,M,W,clo) {
                          ct$assign("ta", as.array(t))
                          ct$assign("rh", as.array(rh))
-                         ct$assign("vel", as.array(wind))
+                         ct$assign("wind", as.array(wind))
                          ct$assign("tr", as.array(tr))
                          ct$assign("M", as.array(M))
                          ct$assign("W", as.array(W))
                          ct$assign("clo", as.array(clo))
-                         ct$eval("var res=[]; for(var i=0, len=ta.length; i < len; i++){ res[i]=PMV_ISO7730(ta[i],rh[i],vel[i],tr[i],M[i],W[i],clo[i])};")
+                         ct$eval("var res=[]; for(var i=0, len=ta.length; i < len; i++){ res[i]=PMV_ISO7730(ta[i],rh[i],wind[i],tr[i],M[i],W[0],clo[0])};")
                          res=ct$get("res")
                          return(ifelse(res==9999,NA,res))
 }

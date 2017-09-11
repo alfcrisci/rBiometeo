ba1a9c8621150ba6fdda5be62c8b7f99d9e0684c
Alfonso <alfcrisci@gmail.com>
2017-09-11 15:15:23 +0200

    Update steadman_class.r

--- R/steadman_class.r
+++ R/steadman_class.r
@@ -19,7 +19,7 @@ steadman_class=function(steadman_index) {
                          
                          res=ifelse(is.na(steadman_index),
                                     NA,
-                                    as.numeric(cut(steadman_index, c(-100,-5,0,5,10,27,30,35,40,100),left=T,label=c(1:9))))
+                                    as.numeric(cut(steadman_index, c(-100,-4.99,0.01,5.01,10.01,27.01,30.01,35.01,40.01,100),right=T,label=c(1:9))))
                          return(res)
 }
 

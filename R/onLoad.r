f4957db1d456dfdc64b91e07b227586b227828a1
Alfonso <alfcrisci@gmail.com>
2019-11-29 16:45:44 +0100

    Update onLoad.r

--- R/onLoad.r
+++ R/onLoad.r
@@ -5,7 +5,7 @@
 ct <- NULL
 
 .onLoad <- function(libname, pkgname){
-  ct <<- V8::v8()
-  ct$source(system.file("inst/js/biometeo.js", package = pkgname))
+  ct <<- V8::new_context()
+  ct$source(system.file("js/biometeo.js", package = pkgname))
 
 }

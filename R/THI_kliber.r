16a415ce9bb2f8f56d82f72c827d9a6fb99573d1
Alfonso <alfcrisci@gmail.com>
2024-01-09 17:02:35 +0100

    Update THI_kliber.r

--- R/THI_kliber.r
+++ R/THI_kliber.r
@@ -1,4 +1,4 @@
-#' THI_kliber
+#' THI_kibler
 #'
 #' Calculate Temperature Humidity Index following Kliber equantions. Adopted by Sardinia ARPA.
 #'
@@ -6,12 +6,12 @@
 #' @param rh numeric  Relative humidity in percentage.
 #' @return thi index
 #' @author Istituto per la Bioeconomia CNR Firenze Italy  Alfonso Crisci \email{alfonso.crisci@@ibe.cnr.it}
-#' 
+#' @references Kibler, H.H. Thermal effects of various temperature-humidity combinations on Holstein cattle as measured by eight physiological responses. Res. Bull. 1964, 862, 1–42. 
 #' @export
 #'
 
 
-THI_kliber=function(t,rh) {
+THI_kibler=function(t,rh) {
                          ct$assign("t", as.array(t))
                          ct$assign("rh", as.array(rh))
                          ct$eval("var res=[]; for(var i=0, len=t.length; i < len; i++){ res[i]=THI_kliber(t[i],rh[i])};")
